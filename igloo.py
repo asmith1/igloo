b7b9fef111081f507e144735f48a0b9fe3b01461
Ashley Smith <Ashley@Ashleys-MacBook-Pro-6.local>
2016-12-22 23:56:38 -0500

    tried to add search algorithm

--- igloo.py
+++ igloo.py
@@ -1,15 +1,26 @@
-print "This is my first python program!"
-#a = input("Please enter a number: ")
-#b = input("Please enter another number: ")
-#print 'the sum of your numbers is', a+b
+def search(searchName):
+	results = []
+	if hookups.has_key(searchName) == False:
+		print "You are not in our database. Join by typing add!"
+	else:
+		names = hookups.get(searchName)
+		for i in range(len(names)):
+			if hookups.has_key(names[i]): #that name has entered hookups
+				# add all of the values associated with that name key
+				results = results + hookups.get(names[i])
+				print "Results from first if statement are:", results
+			for ind in range(len(hookups.keys())):
+				for index in range(len(hookups.get(hookups.keys()[ind]))): #for each of the names in the list of name associated with key[ind]
+					if hookups.get(hookups.keys()[ind])[index] == searchName:
+						results.append(hookups.keys()[ind])
+						print "Results from iteration", index, "are :", results
+		print "Your eskimo siblings are: ", results
 
 hookups = {}
 while True:
 	mode = raw_input("Would you like to add hookups or search for your Eskimo brothers and sisters? ")
 	if mode == "add":
-		#hookups = {}
 		while True:
-			i = 0
 			names = []
 			yourname = raw_input("Please enter your name: ")
 			if yourname == 'done':
@@ -17,7 +28,6 @@ while True:
 			if hookups.has_key(yourname):
 				# just append the list of names to the names that are already there
 				names = hookups.get(yourname) #returns all the names associated with that name
-			#else:
 			newHookups = {yourname: names}
 			hookups.update(newHookups)
 			while True:
@@ -25,19 +35,15 @@ while True:
 				if name == 'done':
 					break
 				names.append(name)
-				print names
+				#print names
 				print hookups
-				i = i+1
 	elif mode == "search":
-		#do the searching algorithm here
-		print "searching algorithm activated"
 		searchName = raw_input("Please enter your name: ")
-		if hookups.has_key(searchName) == False:
-			print "You are not in our database. Join by typing add!"
-		elif:
-
+		search(searchName)
 	elif mode == 'exit':
 		break
 	else:
 		print "Please type 'add', 'search', or 'exit'"
+
+
 
